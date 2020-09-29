3c94b1c11ec7390ab3cae75202d8e706d38ad22e
IvannaHrytsiuk <izink0.0@gmail.com>
2020-09-29 15:29:55 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+    if(n>0){
+        n = n + "";
+        return n.split("").reverse().join("");
+    } else{
+        n = n.toString().substring(1);
+        n = n + "";
+        return n.split("").reverse().join("");
+    }
+    
 }
